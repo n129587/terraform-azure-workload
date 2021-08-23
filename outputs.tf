e83b479af84b3e3af3ef7d1970ed58ed721d7a29
Jorge <georgevazj@gmail.com>
2021-08-23 11:41:53 +0200

    Fixed outputs

--- outputs.tf
+++ outputs.tf
@@ -1,27 +1,27 @@
 output "resource_group_id" {
-  value = azurerm_resource_group.rsg.id
+  value = module.rsg.id
 }
 
 output "resource_group_name" {
-  value = azurerm_resource_group.rsg.name
+  value = module.rsg.name
 }
 
 output "resource_group_location" {
-  value = azurerm_resource_group.rsg.location
+  value = module.rsg.location
 }
 
 output "akv_id" {
-  value = azurerm_key_vault.akv.id
+  value = module.akv.id
 }
 
 output "akv_name" {
-    value = azurerm_key_vault.akv.name
+    value = module.akv.name
 }
 
 output "sta_id" {
-  value = azurerm_storage_account.sta.id
+  value = module.sta.id
 }
 
 output "sta_name" {
-  value = azurerm_storage_account.sta.name
+  value = module.sta.name
 }
