be746ce587f5678af236b0c69c912244d242fe30
Jorge <georgevazj@gmail.com>
2021-08-23 11:58:02 +0200

    Fixed outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,27 +1,27 @@
 output "resource_group_id" {
-  value = module.rsg.id
+  value = module.rsg.resource_group_id
 }
 
 output "resource_group_name" {
-  value = module.rsg.name
+  value = module.rsg.resource_group_name
 }
 
 output "resource_group_location" {
-  value = module.rsg.location
+  value = module.rsg.resource_group_location
 }
 
 output "akv_id" {
-  value = module.akv.id
+  value = module.akv.akv_id
 }
 
 output "akv_name" {
-    value = module.akv.name
+    value = module.akv.akv_name
 }
 
 output "sta_id" {
-  value = module.sta.id
+  value = module.sta.sta_id
 }
 
 output "sta_name" {
-  value = module.sta.name
+  value = module.sta.sta_name
 }
