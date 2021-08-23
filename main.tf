2f3b1999dbd8e606e26b9b7333b4bb1af240e1c7
Jorge <georgevazj@gmail.com>
2021-08-23 11:31:19 +0200

    Fixed module dependencies

--- main.tf
+++ main.tf
@@ -27,7 +27,6 @@ module "akv" {
   source  = "app.terraform.io/georgevazj-lab/akv/azure"
   version = "0.0.8"
 
-  akv_location = module.rsg.outputs.resource_group_location
   akv_name = var.akv_name
   enabled_disk_encryption = var.enabled_disk_encryption
   purge_protection_enabled = var.purge_protection_enabled
@@ -41,7 +40,6 @@ module "sta" {
   source  = "app.terraform.io/georgevazj-lab/sta/azure"
   version = "0.0.4"
 
-  resource_group_name = module.rsg.resource_group_name
   sta_location = module.rsg.outputs.resource_group_location
   sta_name = var.sta_name
   account_replication_type = var.account_replication_type
