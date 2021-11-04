7a6d3d35faa57711b95b09afa7e484e804f4d1b3
Víctor Torres <victormanuel.torreshernando@gruposantander.es>
2021-11-04 11:28:26 +0100

    changes to deploy with cli

--- .terraform.lock.hcl
+++ .terraform.lock.hcl
@@ -6,6 +6,7 @@ provider "registry.terraform.io/hashicorp/azurerm" {
   constraints = "2.46.0"
   hashes = [
     "h1:5l0opMFF2DRFPnm4pRscRCRVYa8M9fuQv4BA0pOur4I=",
+    "h1:lWovJczej2IP+aL24Fs4mz6brz7eoMxuqzxhEa2I/rc=",
     "zh:049938dab6f78a642a7c4147f99f146dc25824c738d0b18d448c665880d105fa",
     "zh:139de4a46bbfc18b5403fa2264878a056fd8593d860942233913e54233c7327d",
     "zh:19ceb7fc964265cb7e7f708e32b72a00089ed0398ae1f0014b5832078bd5be79",
--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "=2.46.0"
     }
   }
+  backend "remote" {
+    organization = "sanesp-poc"
+
+    workspaces {
+      name = "terraform-azure-workload-cli"
+    }
+  }
 }
 
 # Configure the Microsoft Azure Provider
@@ -18,43 +25,43 @@ module "builder" {
   source  = "app.terraform.io/sanesp-poc/builder/azure"
   version = "0.0.1"
 
-  sequence_number = var.sequence_number
+  sequence_number  = var.sequence_number
   workload_acronym = var.workload_acronym
-  location = var.location
-  environment = var.env_acronym
-  entity = var.entity
+  location         = var.location
+  environment      = var.env_acronym
+  entity           = var.entity
 }
 
 module "rsg" {
   source  = "app.terraform.io/sanesp-poc/rsg/azure"
   version = "0.0.3"
 
-  resource_group_name = module.builder.resource_group
+  resource_group_name     = module.builder.resource_group
   resource_group_location = var.location
-  description = "Resource group from Terraform Cloud"
+  description             = "Resource group from Terraform Cloud"
 }
 
 module "akv" {
   source  = "app.terraform.io/sanesp-poc/akv/azure"
   version = "0.0.15"
 
-  akv_name = module.builder.akv_name
-  location = module.rsg.resource_group_location
-  enabled_disk_encryption = var.enabled_disk_encryption
-  purge_protection_enabled = var.purge_protection_enabled
-  sku_name = var.sku_name
+  akv_name                   = module.builder.akv_name
+  location                   = module.rsg.resource_group_location
+  enabled_disk_encryption    = var.enabled_disk_encryption
+  purge_protection_enabled   = var.purge_protection_enabled
+  sku_name                   = var.sku_name
   soft_delete_retention_days = var.soft_delete_retention_days
-  resource_group_name = module.rsg.resource_group_name
+  resource_group_name        = module.rsg.resource_group_name
 }
 
 module "sta" {
   source  = "app.terraform.io/sanesp-poc/sta/azure"
   version = "0.0.9"
 
-  sta_name = module.builder.sta_name
-  location = module.rsg.resource_group_location
+  sta_name                 = module.builder.sta_name
+  location                 = module.rsg.resource_group_location
   account_replication_type = var.account_replication_type
-  account_tier = var.account_tier
-  environment_tag = var.environment_tag
-  resource_group_name = module.rsg.resource_group_name
+  account_tier             = var.account_tier
+  environment_tag          = var.environment_tag
+  resource_group_name      = module.rsg.resource_group_name
 }
