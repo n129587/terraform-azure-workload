60572e8aa67982efed0765c2fcb86d35e17672ca
Víctor Torres <victormanuel.torreshernando@gruposantander.es>
2021-11-17 15:44:24 +0100

    wip

--- main.tf
+++ main.tf
@@ -2,12 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=2.46.0"
+      version = "~> 2.46.0"
     }
   }
   backend "remote" {
     organization = "sanesp-poc"
-
     workspaces {
       name = "terraform-azure-workload-cli"
     }
@@ -17,6 +16,10 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {
+    key_vault {
+      recover_soft_deleted_key_vaults = true
+      purge_soft_delete_on_destroy = true
+    }
   }
 }
 
